561b7f0f8712a91267d61ac5b31d1d813382fd45
Atchutarao125 <163500134+Atchutarao125@users.noreply.github.com>
2024-05-03 10:46:09 +0530

    Update GoogleStepDef.java

--- src/test/java/STEPDEF/GoogleStepDef.java
+++ src/test/java/STEPDEF/GoogleStepDef.java
@@ -24,7 +24,7 @@ public class GoogleStepDef {
 	   if(actT.contains("Google")) {
 		   System.out.println("TC IS PASS");
 		   System.out.println("Atchutarao");
-		   System.out.println("vicky";
+		   System.out.println("vicky");
 	   }
 	}
 
